8851da5a41a3109edba158dc253e067233362d08
Anders Børjesson <anbo@zealand.dk>
2021-02-13 19:57:44 +0100

    Refactor: migrate to androidx

--- app/src/main/java/dk/easj/anbo/autocompletetextviewexample/MainActivity.java
+++ app/src/main/java/dk/easj/anbo/autocompletetextviewexample/MainActivity.java
@@ -1,7 +1,7 @@
 package dk.easj.anbo.autocompletetextviewexample;
 
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.text.Editable;
